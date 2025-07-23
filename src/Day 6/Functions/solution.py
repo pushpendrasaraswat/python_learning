faf28d5f4aee3b6d64d97adece102e85db7d1232
pushpendrasaraswat <pushpendra.saraswat@gmail.com>
2025-07-23 13:26:38 +0530

    Day 6
    function

---
+++ src/Day 6/Functions/solution.py
@@ -0,0 +1,20 @@
+def my_function():
+    print("Hello")
+    print("Bye")
+
+
+my_function()
+
+
+# return function
+
+def say_hello():
+    return "Hello"
+
+print(say_hello())
+
+# sum 2 values
+def sum_two_values(a, b):
+    return a + b
+
+print(sum_two_values(1, 2))

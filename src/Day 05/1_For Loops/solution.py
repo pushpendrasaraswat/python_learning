896b08739a71d4885193453cd0b15b3f665b263d
pushpendrasaraswat <pushpendra.saraswat@gmail.com>
2025-07-24 17:03:10 +0530

    loop
    loop

--- src/Day 05/1_For Loops/solution.py
+++ src/Day 05/1_For Loops/solution.py
@@ -3,3 +3,8 @@ fruits = ["Apple", "Peach", "Pear"]
 for fruit in fruits:
     print(fruits.index(fruit))
     print(fruit)
+
+
+for _ in range(2): # _ is a common convention for a variable that is not used , if we dont want to use the variable
+    # and want to repeat the loop for 2 times in this case, this is the way to do it
+    print("Hello!")
